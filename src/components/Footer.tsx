7577cdbf56307f9095587adda07ec945ef3658ea
mazen19812002 <53929390+mazen19812002@users.noreply.github.com>
2026-09-26 13:15:59 +0300

    refactor: cleanup codebase and update date logic
    Remove unused components and consolidate logic for calculating date limits in the custom date picker.

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => {
-  return (
-    <footer>
-      All rights are reserved to Dr. Mazen Badawy – Doctor of English Teaching &amp; Testing
-    </footer>
-  );
-};
